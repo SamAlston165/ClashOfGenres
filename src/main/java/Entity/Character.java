5a8da07b0a99c563b44d7b702d5047d00e2a78fb
Nate Reyes <njr9@students.uwf.edu>
2017-11-27 15:47:16 -0600

    Package Change

---
+++ src/main/java/Entity/Character.java
@@ -0,0 +1,11 @@
+package Entity;
+
+public class Character {
+    private int max_hp;
+    private int current_hp;
+    private int lvl;
+    private int attack;
+    private int defense;
+    private Weapons weapon;
+
+}
