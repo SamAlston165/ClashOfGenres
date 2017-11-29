bad9c3a339e4d7f7a290ea8a3b5ebc0d69a40349
Nate Reyes <njr9@students.uwf.edu>
2017-11-29 13:42:09 -0600

    added some background function to character to be merged with user

--- src/main/java/Entity/Character.java
+++ src/main/java/Entity/Character.java
@@ -1,11 +1,66 @@
 package Entity;
 
+
 public class Character {
+    //no magic numbers
+    private static final int death = 0;
+
+    private String race;
+    private ClassType classType;
     private int max_hp;
     private int current_hp;
     private int lvl;
     private int attack;
     private int defense;
     private Weapons weapon;
+    private int movement;
+    private int attackRange;
+    private int avalible; // each place can be a different check  1's 10's etc
+
+    //makes empty character
+    public Character(){
+
+    }
+
+
+    public Character(String r, String ct){
+
+    }
+
+    //get avalible actions for character
+    public int getAvalible(){
+        return avalible;
+    }
+
+    public void setAvalible(int a){
+        avalible = a;
+        return;
+    }
+
+    //get movement for character
+    public int getMovement(){
+        return movement;
+    }
+
+    //get attack range
+    public int getAttackRange(){
+        return attackRange;
+    }
+
+    //change health (attacked'-' or healed'+')
+    public void changeHealth(int num){
+        current_hp = current_hp + num;
+        //check if dead
+        if(current_hp<=death){
+            setAvalible(death);
+            return;
+        }
+        //prevent hp going above max
+        else if(current_hp>max_hp){
+            current_hp = max_hp;
+            return;
+        }
+        return;
+    }
 
 }
