f5a6513f5280d7a83643cf9625b0cc0a2df8f5fc
Daniel <djb58@students.uwf.edu>
2017-12-04 21:17:28 -0600

    User/Char merged, got rid of User. Ai updated to reflect that.

--- src/main/java/AI/AI.java
+++ src/main/java/AI/AI.java
@@ -1,6 +1,6 @@
 package AI;
 
-import Entity.User;
+import Entity.Character;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,21 +9,21 @@ public class AI {
 
     private static List<AInfo> info;
 
-    public static void decide(User enemy, List<User> targets){
+    public static void decide(Character enemy, List<Character> targets){
         collect(enemy, targets);
     }
 
 
     //FUNCTION TO COLLECT RELEVANT INFO FROM ALL TARGETS
-    public static void collect(User enemy, List<User> targets){
+    public static void collect(Character enemy, List<Character> targets){
 
         info = new ArrayList<AInfo>();
         double distance;
         int threat;
         boolean range;
 
-        //RUN THROUGH EACH USER IN THE TARGET GROUP TO COLLECT RELEVANT INFO
-        for(User target : targets) {
+        //RUN THROUGH EACH Character IN THE TARGET GROUP TO COLLECT RELEVANT INFO
+        for(Character target : targets) {
             distance = getDistance(enemy, target);
             range = inRange(enemy, distance);
             threat = determineThreat(enemy, target);
@@ -37,7 +37,7 @@ public class AI {
     }
 
     //FUNCTION FOR CHECKING TARGETS WITHIN ATTACK RANGE
-    public static boolean inRange(User enemy, double distance){
+    public static boolean inRange(Character enemy, double distance){
         boolean attackRange = false;
         if(enemy.getAttackRange() < distance)
             attackRange = true;
@@ -46,19 +46,19 @@ public class AI {
 
     }
 
-    public static void attack(User enemy, User target){
+    public static void attack(Character enemy, Character target){
 
     }
 
     //FUNCTION FOR CALCULATING DISTANCE BETWEEN ENEMY AND TARGET
-    public static double getDistance(User enemy, User target){
+    public static double getDistance(Character enemy, Character target){
         double X = Math.pow((enemy.getX() - target.getX()), 2.0);
         double Y = Math.pow((enemy.getY()- target.getY()), 2.0);
 
         return Math.sqrt(X+Y);
     }
 
-    public static int determineThreat(User enemy, User target){
+    public static int determineThreat(Character enemy, Character target){
         int threat = 0;
 
         return threat;
